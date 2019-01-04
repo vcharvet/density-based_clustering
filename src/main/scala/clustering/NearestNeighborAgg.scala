a2fc2a56665a0b370d67510f490fd41e7fd55323
vcharvet <valentin.charvet@telecom-paristech.fr>
2019-01-04 11:46:50 +0100

    [clustering] omptimization of merge in aggregation

--- src/main/scala/clustering/NearestNeighborAgg.scala
+++ src/main/scala/clustering/NearestNeighborAgg.scala
@@ -1,94 +1,14 @@
 package clustering
 
-import org.apache.spark.sql.catalyst.expressions.GenericRowWithSchema
 import org.apache.spark.sql.expressions.Aggregator
-import org.apache.spark.sql.{Encoder, Encoders, Row, TypedColumn}
-import org.apache.spark.sql.expressions.{MutableAggregationBuffer, UserDefinedAggregateFunction}
-import org.apache.spark.sql.types._
+import org.apache.spark.sql.{Encoder, Encoders, Row}
 
 
-/** user defined aggregation function to fetch nearest neighbors of a collection of
-	* points in a dataframe
-	*
-	* @deprecated use  other class instead
-	* It is suposed to be used for a dataframe (i, j, distance(i,j)) grouped by key i
-	* NearestNeighborAgg fetches the kth nearest neighbor in each group and its distance
-	*
-	*/
-class NearestNeighborAgg(k: Int, idCol: String, distanceCol: String) extends UserDefinedAggregateFunction {
-	// the input rows are (j, dist(i, j))
-	def inputSchema: StructType = StructType(
-		StructField(idCol, LongType) :: StructField(distanceCol, DoubleType) :: Nil)
-
-		//TODO find better implementation, with Params ??
-
-
-	/* override bufferSchema
-		sortedDistance contains the sequence of distances to i
-		keyVales map(vector2Id to distance(vector1, vector2)
-	 */
-	def bufferSchema: StructType = {
-		StructType(StructField("sortedDistances", ArrayType(DoubleType))
-		:: StructField("keyValues", MapType(DoubleType, LongType)) :: Nil)}
-
-	//type of the returned value
-	def dataType: DataType = MapType(LongType, DoubleType) // TODO change
-
-	override def deterministic: Boolean = true
-
-	def initialize(buffer: MutableAggregationBuffer): Unit = {
-		buffer(0) = Seq[Double]()
-		buffer(1) = Map[Double, Long]()
-		}
-
-	def update(buffer: MutableAggregationBuffer, input: Row): Unit = {
-		if (!input.isNullAt(0)) {
-
-			val newDist: Double = input.getAs[Double](1)
-			val newId: Long = input.getAs[Long](0)
-
-			val updatedDistances = this.sortInsert(buffer.getAs[Seq[Double]](0), newDist)
-
-			buffer(0) = updatedDistances
-			buffer(1) = buffer.getAs[Map[Double, Long]](1) + ((newDist, newId))
-		}
-	}
-
-	def merge(buffer1: MutableAggregationBuffer, buffer2: Row): Unit = {
-		// update key values
-		buffer1(1) = buffer1.getAs[Map[Double, Long]](1) ++ buffer2.getAs[Map[Double, Long]](1)
-
-		// update distances
-		var distances = buffer2.getAs[Seq[Double]](0)
-		for (newDistance1 <- buffer1.getSeq(0).iterator){
-			distances = sortInsert(distances, newDistance1)
-		}
-		buffer1(0) = distances
-	}
-
-	def evaluate(buffer: Row): Map[Long, Double] = { //change return type to Map(Long, Double)
-		val kDist = buffer.getAs[Seq[Double]](0).apply(k - 1)
-		val kNeighbor = buffer.getAs[Map[Double, Long]](1).apply(kDist)
-
-		Map[Long, Double]((kNeighbor, kDist))
-	}
-
-	//TODO how to unit test private function?
-	def sortInsert(currentDistances: Seq[Double], newDist: Double): Seq[Double] = {
-	currentDistances match {
-		case head +: tail => if (newDist < head)  Seq[Double](newDist +: head +:  tail: _*)
-			else head +: sortInsert(tail, newDist)
-		case head +: Nil =>  Seq[Double](head, newDist)
-		case Nil => Seq(newDist)
-		}
-	}
-}
-
-case class GroupRow(j: Long, distIJ: Double) //extends GenericRowWithSchema
+case class GroupRow(j: Long, distIJ: Double) //@deprecated
 case class GroupBuffer(sortedList: Seq[(Long, Double)])
 
 //@TODO add parameter, finish different for 'optics' or 'hdbscan"
-class NearestNeighborAgg2(k: Int, colName: String) extends Aggregator[Row, GroupBuffer, (Long, Double)]
+class NearestNeighborAgg(k: Int, idCol: String, distCol: String) extends Aggregator[Row, GroupBuffer, (Long, Double)]
 	with Serializable {
 	override def bufferEncoder: Encoder[GroupBuffer] = Encoders.kryo[GroupBuffer]
 
@@ -98,29 +18,23 @@ class NearestNeighborAgg2(k: Int, colName: String) extends Aggregator[Row, Group
 	override def zero: GroupBuffer = GroupBuffer(Seq[(Long, Double)]())
 
 	override def merge(b1: GroupBuffer, b2: GroupBuffer): GroupBuffer = {
-	//TODO optimize merge
-		var sortedTuples = Seq[(Long, Double)]()
-
-		for (x <- b1.sortedList){sortedTuples = sortInsert(sortedTuples, x)}
-		for (x <- b2.sortedList){sortedTuples = sortInsert(sortedTuples, x)}
+//		val sortedTuples = (b1.sortedList ++ b2.sortedList).sortBy(_._2)
+		val sortedTuples = mergeSeq[Long](b1.sortedList, b2.sortedList)
 
 		GroupBuffer(sortedTuples)
 	}
 
 	override def reduce(buffer: GroupBuffer, input: Row): GroupBuffer = {
-		val castedRow = input.getAs[Row](colName)
-		GroupBuffer(sortInsert(buffer.sortedList,
-			(castedRow.getAs[Long](0), castedRow.getAs[Double](1))))
+		val id = input.getAs[Long](idCol)
+		val dist = input.getAs[Double](distCol)
+		GroupBuffer(sortInsert(buffer.sortedList, (id, dist) ))
 	}
 
 	override def finish(buffer: GroupBuffer): (Long, Double) = {
-//		GroupRow(buffer.sortedList(k-1)._1, buffer.sortedList(k-1)._2)
 		// retrieve kth-nearest neighbor and distance
 		val couple = buffer.sortedList(k-1)
-//		Map[Long, Double]((couple._1, couple._2))
 		couple
 	}
-
 
 	def sortInsert(currentList: Seq[(Long, Double)], newTuple: (Long, Double)): Seq[(Long, Double)] = {
 		currentList match {
@@ -129,6 +43,18 @@ class NearestNeighborAgg2(k: Int, colName: String) extends Aggregator[Row, Group
 				else newTuple +: head +: tail
 			case head +: Nil => Seq(head, newTuple)
 			case Nil => Seq(newTuple)
+		}
+	}
+
+	def mergeSeq[U](seq1: Seq[(U, Double)], seq2: Seq[(U, Double)]): Seq[(U, Double)] = {
+		(seq1, seq2) match {
+			case (Nil, Nil) => Nil
+			case (Nil, seq) => seq
+			case (seq, Nil) => seq
+			case (head1 +: tail1 , head2 +: tail2) => {
+				if (head1._2 <= head2._2) Seq(head1, head2) ++ mergeSeq[U](tail1, tail2)
+				else Seq(head2, head1) ++ mergeSeq[U](tail1, tail2)
+				}
 		}
 	}
 
--- src/test/scala/clustering/NeighborsAgregatorTest.scala
+++ src/test/scala/clustering/NeighborsAgregatorTest.scala
@@ -8,69 +8,6 @@ import org.scalatest.FlatSpec
 
 class NeighborsAgregatorTest extends FlatSpec {
 	Logger.getLogger("org").setLevel(Level.WARN)
-  Logger.getLogger("akka").setLevel(Level.WARN)
-
-  val ss = SparkSession.builder
-    .appName("clustering.CoreDistance Unit Test")
-    .master("local[1]")
-    .getOrCreate()
-
-  import ss.implicits._
-
-	val df = ss.sparkContext.parallelize(Seq(
-		(3l, 4l, 4d),
-		(3l, 5l, 1d),
-		(4l, 3l, 4d),
-		(4l, 5l, 5d),
-		(5l, 4l, 1d),
-		(5l, 4l, 5d)))
-  	.toDF("i", "j", "distIJ")
-
-	// group of point 4
-	val dfGroup = ss.sparkContext.parallelize(Seq(
-		(4l, 4d),
-		(5l, 1d)))
-  	.toDF("j", "distIJ")
-
-	val grouped_df = df.groupBy("i")
-
-	"Sorting distances" should "yield" in {
-		val sequence = Seq(1d, 2, 3, 4, 6, 7)
-		val udfAgg = new NearestNeighborAgg(2, "j", "distIJ")
-		val computed = udfAgg.sortInsert(sequence, 5d)
-
-		val expected = Seq(1d, 2, 3, 4, 5d, 6, 7)
-
-		assertResult(expected)(computed)
-	}
-
-	"Aggregator on small df" should "yield" in {
-		val udfAgg = new NearestNeighborAgg(2, "j", "distIJ")
-		val resCol = dfGroup.select(udfAgg($"j", $"distIJ"))
-
-		assertResult(Map((4l, 4d)))(resCol.head().get(0))
-		}
-
-	"Aggregator on 2 nearest neighbors" should "yield" in{
-		val udfAgg = new NearestNeighborAgg(2, "j", "distIJ")
-
-		val computed = grouped_df.agg(udfAgg($"j", $"distIJ").alias("kNeighborDistance") )
-  			.orderBy("i")
-
-		computed.show()
-
-		val expected = ss.sparkContext.parallelize(Seq(
-			(3l, Map[Long, Double]((4l, 4d))),
-			(4l, Map[Long, Double]((5l, 5d))),
-			(5l, Map[Long, Double]((4l, 5d))))).toDF("j", "2NNeighbor")
-
-		assertResult(expected.collect()  )(computed.collect())
-		}
-}
-
-
-class NeighborsAgregator2Test extends FlatSpec {
-	Logger.getLogger("org").setLevel(Level.WARN)
 	Logger.getLogger("akka").setLevel(Level.WARN)
 
 	val ss = SparkSession.builder
@@ -87,26 +24,17 @@ class NeighborsAgregator2Test extends FlatSpec {
 		(4l, 5l, 5d),
 		(5l, 3l, 1d),
 		(5l, 4l, 5d)))
-  	.map(row => (row._1, GroupRow(row._2, row._3)))
-  	.toDF("i", "distIJ")
-//  	.map(row => (row.get(0), GroupRow(row.getAs[Long](1), row.getAs[Double](2))))
-//  	.withColumnRenamed("_1", "i")
-//  	.withColumnRenamed("_2", "distIJ")
-  	.as[(Long, GroupRow)]
+		.toDF("i", "j", "distIJ")
+//  	.map(row => (row._1, GroupRow(row._2, row._3)))
+//  	.toDF("i", "distIJ")
 
 	ds.show()
 
-	// group of point 4
-	val dfGroup = ss.sparkContext.parallelize(Seq(
-		(4l, 4d),
-		(5l, 1d)))
-		.toDF("j", "distIJ")
 
-//	val grouped_df = df.groupBy("i")
 
 	"Sorting distances" should "yield" in {
 		val sequence = Seq[(Long, Double)]( (0l,1d), (1l,2), (2l,3), (3l,4), (4l,6), (5l,7))
-		val udfAgg = new NearestNeighborAgg2(2, "distIJ")
+		val udfAgg = new NearestNeighborAgg(2, "j", "distIJ")
 		val computed = udfAgg.sortInsert(sequence, (6l,5d))
 
 		val expected = Seq((0l,1d), (1l,2), (2l,3), (3l,4), (6l,5d), (4l,6), (5l,7))
@@ -114,19 +42,12 @@ class NeighborsAgregator2Test extends FlatSpec {
 		assertResult(expected)(computed)
 	}
 
-	"Aggregator on small df" should "yield" in {  //delete this test
-		val udfAgg = new NearestNeighborAgg2(2, "distIJ").toColumn
-		val resCol = dfGroup.as[GroupRow].select(udfAgg)
-
-		assertResult((4l, 4d))(resCol.head())
-	}
 
 	"Aggregator on 2 nearest neighbors" should "yield" in{
 //		println("Test not yet implemented")
-		val udfAgg = new NearestNeighborAgg2(2, "distIJ")
+		val udfAgg = new NearestNeighborAgg(2, "j", "distIJ")
 
 
-//		val computed = ds.groupByKey(_._1). //.alias("kNeighborDistance"))
 		val computed = ds.groupBy($"i").agg(udfAgg.toColumn as "coreDist")
 			.orderBy("i")
 
